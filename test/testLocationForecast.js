1f7c71bffce31a31451b395112a01ebac83ddc93
Evan Shortiss <evan.shortiss@feedhenry.com>
2013-11-08 11:55:31 +0000

    Update to provide simple interface and no wrapping of objects

--- test/testLocationForecast.js
+++ test/testLocationForecast.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
 var api = require('../index.js');
+var fs = require('fs');
 
+var FILENAME = 'r-e-s.txt';
 var DUBLIN = {
   lat: 53.3478,
   lon: 6.2597
@@ -9,41 +11,20 @@ var DUBLIN = {
 describe('Test location forecast function', function() {
   this.timeout(10000);
 
-  // it('getForecastForTime(), Should return a valid forecast object', function(done) {
-  //   api.locationForecast(DUBLIN, function(err, weather) {
-  //     assert(!err);
-  //     assert(typeof weather === 'object');
-  //     weather.getForecastForTime(new Date(), function(err, res) {
-  //       assert(!err);
-  //       assert(res);
-  //       done();
-  //     });
-  //   });
-  // });
-
-  // it('getCurrentSummary(), Should return the current weather', function(done) {
-  //   api.locationForecast(DUBLIN, function(err, weather) {
-  //     assert(typeof weather === 'object');
-  //     weather.getCurrentSummary(function(err, res) {
-  //       assert(!err);
-  //       assert(res);
-  //       done();
-  //     });
-  //   });
-  // });
-
-  it('getFiveDaySummary(), Should return 5 days of weather', function(done) {
-    api.locationForecast(DUBLIN, function(err, weather) {
+  it('Should return response from API using default version', function(done) {
+    api.locationforecast(DUBLIN, function(err, weather) {
       assert(!err);
       assert(weather);
-      weather.getFiveDaySummary(function(err, res) {
-        console.log(err);
-        assert(!err);
-        assert(res);
-
-        console.log(res);
-        done();
-      });
+      done();
     });
   });
+
+  it('Should pipe response to file', function(done) {
+    var stream = fs.createWriteStream(FILENAME);
+    stream.on('close', function() {
+      fs.unlink('./'+FILENAME);
+      done();
+    });
+    api.locationforecast(DUBLIN, stream);
+  })
 })
